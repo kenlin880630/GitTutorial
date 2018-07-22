85aaf271d3851887ad96c8df520304ab50d201c8
林文約 <kenlin@linwenyuede-macbook-pro.local>
2018-07-22 20:28:18 +0800

    Add another print.

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("Hello CS193p")
         print("Hello world!")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
